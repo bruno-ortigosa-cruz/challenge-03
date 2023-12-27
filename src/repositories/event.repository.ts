da396021045ce25f27135c2790de0355fcf164a4
Bruno Ortigosa Cruz <brunortigosa@gmail.com>
2023-12-27 19:30:31 -0300

    feat: add event repository

---
+++ src/repositories/event.repository.ts
@@ -0,0 +1,17 @@
+import { IEvent, IReturnEvent } from '../interfaces/event.interface';
+import { EventModel } from '../models/event.model';
+
+export class EventRepository {
+    constructor() {}
+
+    public async getAllEvents(): Promise<IEvent[]> {
+        return await EventModel.find({});
+    }
+
+    public async createEvent(payload: IEvent): Promise<IReturnEvent> {
+        console.log(payload);
+        const algo = await EventModel.create(payload);
+        console.log(algo)
+        return algo
+    }
+}
