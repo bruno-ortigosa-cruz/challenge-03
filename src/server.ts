a694e78efbab3f558a07ff8479916df82d84066b
Bruno Ortigosa Cruz <brunortigosa@gmail.com>
2023-12-27 19:34:11 -0300

    feat: add server class

---
+++ src/server.ts
@@ -0,0 +1,38 @@
+import 'dotenv/config';
+import { IEnv } from './interfaces/env.interface';
+import { cleanEnv, port, str } from 'envalid';
+import { App } from './app';
+import { Database } from './infra/database.infra';
+
+class Server {
+    private prefix: string = '/api/v1';
+    private env: IEnv;
+    private express: App;
+    private database: Database;
+
+    constructor() {
+        this.env = this.validateEnv();
+        this.express = new App(this.prefix);
+        this.database = new Database(this.env.MONGO_URI);
+    }
+
+    public start() {
+        this.express.app.listen(this.env.PORT, async () => {
+            await this.database.connect().then(() => {
+                console.log(`Server is listening on port ${this.env.PORT}...`);
+            });
+        });
+    }
+
+    private validateEnv(): IEnv {
+        const env = cleanEnv(process.env, {
+            MONGO_URI: str(),
+            PORT: port({ default: 3000 }),
+        }) as IEnv;
+
+        return env;
+    }
+}
+
+const server = new Server();
+server.start();
