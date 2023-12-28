3938f9dd9242354c5791ce6639b4cad4432af1f2
Bruno Ortigosa Cruz <brunortigosa@gmail.com>
2023-12-28 02:11:00 -0300

    feat: add remove event method

--- src/services/event.service.ts
+++ src/services/event.service.ts
@@ -1,4 +1,4 @@
-import { IEvent } from '../interfaces/event.interface';
+import { IEvent, TypeDayOfWeek } from '../interfaces/event.interface';
 import { EventRepository } from '../repositories/event.repository';
 
 export class EventService {
@@ -6,15 +6,19 @@ export class EventService {
 
     constructor() {}
 
-    public async getEvents(weekdayQuery: string): Promise<IEvent[]> {
+    public async get(weekdayQuery: TypeDayOfWeek): Promise<IEvent[]> {
         if (!weekdayQuery) {
-            return await this.eventRepository.getAllEvents();
+            return await this.eventRepository.getAll();
         } else {
-            return await this.eventRepository.getQueryEvents(weekdayQuery);
+            return await this.eventRepository.getQuery(weekdayQuery);
         }
     }
 
     public async createEvent(payload: IEvent) {
-        return await this.eventRepository.createEvent(payload);
+        return await this.eventRepository.create(payload);
+    }
+
+    public async remove(weekdayQuery: TypeDayOfWeek) {
+        return await this.eventRepository.remove(weekdayQuery);
     }
 }
