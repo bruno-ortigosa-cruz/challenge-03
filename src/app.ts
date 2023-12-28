598bc98f55846e5c7302247f784e4ed390705c7f
Bruno Ortigosa Cruz <brunortigosa@gmail.com>
2023-12-28 19:06:01 -0300

    feat: add user routes to app

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,22 @@
 import 'express-async-errors';
 import express from 'express';
 import { EventRoutes } from './infra/routes/event.route';
+import { UserRoutes } from './infra/routes/user.route';
 
 export class App {
     public readonly app = express();
-    private eventRoute;
+    private eventRoute: EventRoutes;
+    private userRoute: UserRoutes;
     private prefix: string;
 
     constructor(prefix: string) {
         this.prefix = prefix;
         this.eventRoute = new EventRoutes();
+        this.userRoute = new UserRoutes();
 
         this.app.use(express.json());
 
         this.app.use(this.prefix, this.eventRoute.router);
+        this.app.use(this.prefix, this.userRoute.router);
     }
 }
