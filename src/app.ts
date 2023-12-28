48770512d082fca94694c50d119a222d1544be82
Bruno Ortigosa Cruz <brunortigosa@gmail.com>
2023-12-28 20:16:04 -0300

    feat: add fallback middleware for non-existing routes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import 'express-async-errors';
 import express from 'express';
 import { EventRoutes } from './infra/routes/event.route';
 import { UserRoutes } from './infra/routes/user.route';
+import { EndpointNotFound } from './helpers/middlewares/not-found.middleware';
 
 export class App {
     public readonly app = express();
@@ -18,5 +19,7 @@ export class App {
 
         this.app.use(this.prefix, this.eventRoute.router);
         this.app.use(this.prefix, this.userRoute.router);
+
+        this.app.use(new EndpointNotFound().fallback);
     }
 }
