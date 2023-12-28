aeb8156170e1a7b10206e714cb7df0924437b743
Bruno Ortigosa Cruz <brunortigosa@gmail.com>
2023-12-28 14:50:59 -0300

    feat: set get by id route

--- src/infra/routes/event.route.ts
+++ src/infra/routes/event.route.ts
@@ -5,6 +5,7 @@ export class EventRoutes {
     public readonly router: Router = Router();
     private controller: EventController;
     private readonly generalRoute = '/events';
+    private readonly idRoute = '/events/:id';
 
     constructor() {
         this.controller = new EventController();
@@ -12,7 +13,8 @@ export class EventRoutes {
     }
 
     private setRoute() {
-        this.router.get(this.generalRoute, this.controller.get);
+        this.router.get(this.generalRoute, this.controller.getByDay);
+        this.router.get(this.idRoute, this.controller.getById);
         this.router.post(this.generalRoute, this.controller.create);
         this.router.delete(this.generalRoute, this.controller.remove);
     }
