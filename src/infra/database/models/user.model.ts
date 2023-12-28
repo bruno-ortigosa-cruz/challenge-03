bfdca9dd8f40a23fc849cfaefae9216a75df96af
Bruno Ortigosa Cruz <brunortigosa@gmail.com>
2023-12-28 19:07:12 -0300

    fix: change user model name

--- src/infra/database/models/user.model.ts
+++ src/infra/database/models/user.model.ts
@@ -2,4 +2,4 @@ import { model } from 'mongoose';
 import { IUser } from '../../../helpers/interfaces/user.interface';
 import { UserSchema } from '../schemas/user.schema';
 
-export const UserModel = model<IUser>('Event', UserSchema);
+export const UserModel = model<IUser>('User', UserSchema);
