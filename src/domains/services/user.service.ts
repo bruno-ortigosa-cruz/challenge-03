bfbfb279d9981bed0165730b5db3b24058105a3f
Bruno Ortigosa Cruz <brunortigosa@gmail.com>
2023-12-28 21:08:12 -0300

    feat: add sign in method

--- src/domains/services/user.service.ts
+++ src/domains/services/user.service.ts
@@ -1,6 +1,12 @@
 import { Request } from 'express';
 import { UserRepository } from '../../infra/repositories/user.repository';
-import { IUserNoPassword } from '../../helpers/interfaces/user.interface';
+import {
+    IUser,
+    IUserNoPassword,
+    IUserSignInRequest,
+    IUserSignInResponse,
+} from '../../helpers/interfaces/user.interface';
+import { compare } from 'bcrypt';
 
 export class UserService {
     private repository: UserRepository;
@@ -10,9 +16,33 @@ export class UserService {
     }
 
     public async signUp(req: Request): Promise<IUserNoPassword> {
-        const payload = req.body;
+        const payload: IUser = req.body;
         const user = (await this.repository.signUp(payload)).toJSON();
         delete user.password;
         return user as IUserNoPassword;
+    }
+
+    public async signIn(req: Request): Promise<IUserSignInResponse> {
+        const payload: IUserSignInRequest = req.body;
+        const user: IUser | null = await this.repository.signIn(payload);
+
+        if (!user) throw new Error('tem esse user não, meu jovem');
+
+        const isPasswordCorrect = await compare(
+            payload.password,
+            user.password,
+        );
+
+        if (!isPasswordCorrect) {
+            throw new Error('ta mexendo onde não deve pitbull');
+        }
+
+        const responseUser: IUserSignInResponse = {
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+        };
+
+        return responseUser;
     }
 }
