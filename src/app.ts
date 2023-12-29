cf5267d574d835af7628031dfb3e8905da786dd3
Bruno Ortigosa Cruz <brunortigosa@gmail.com>
2023-12-29 20:15:43 -0300

    feat: add authentication middleware to event routes

--- src/app.ts
+++ src/app.ts
@@ -3,22 +3,29 @@ import express from 'express';
 import { EventRoutes } from './infra/routes/event.route';
 import { UserRoutes } from './infra/routes/user.route';
 import { EndpointNotFound } from './helpers/middlewares/not-found.middleware';
+import { AuthMiddleware } from './helpers/middlewares/auth.middleware';
 
 export class App {
     public readonly app = express();
     private eventRoute: EventRoutes;
     private userRoute: UserRoutes;
+    private authentication: AuthMiddleware;
     private prefix: string;
 
     constructor(prefix: string) {
         this.prefix = prefix;
-        this.eventRoute = new EventRoutes();
         this.userRoute = new UserRoutes();
+        this.eventRoute = new EventRoutes();
+        this.authentication = new AuthMiddleware();
 
         this.app.use(express.json());
 
-        this.app.use(this.prefix, this.eventRoute.router);
         this.app.use(this.prefix, this.userRoute.router);
+        this.app.use(
+            this.prefix,
+            this.authentication.exec,
+            this.eventRoute.router,
+        );
 
         this.app.use(new EndpointNotFound().fallback);
     }
