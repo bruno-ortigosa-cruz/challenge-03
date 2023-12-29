70b322a6359ef970a508395ae92ed13f53f79eb0
Bruno Ortigosa Cruz <brunortigosa@gmail.com>
2023-12-28 22:41:05 -0300

    feat: add proper filtering for get method

--- src/domains/services/event.service.ts
+++ src/domains/services/event.service.ts
@@ -1,6 +1,7 @@
 import { Request } from 'express';
 import {
     IEvent,
+    IEventQuery,
     IReturnEvent,
     TypeDayOfWeek,
 } from '../../helpers/interfaces/event.interface';
@@ -15,21 +16,40 @@ export class EventService {
 
     public async get(req: Request): Promise<IReturnEvent[] | IReturnEvent> {
         const id: string = req.params.id;
-        const weekdayQuery: TypeDayOfWeek = req.query
-            .dayOfWeek as TypeDayOfWeek;
+        const dayOfWeekQuery = req.query.dayOfWeek as TypeDayOfWeek;
+        const descriptionQuery = req.query.description as string;
 
-        if (id) {
-            const event = await this.repository.getById(id);
-
-            if (!event) {
-                throw new Error('Não achou o evento não meu consagrado');
-            }
-            return event;
+        if (id) this.getById(id);
+        if (!(dayOfWeekQuery || descriptionQuery)) {
+            return await this.repository.getAll();
         }
 
-        if (!weekdayQuery) return await this.repository.getAll();
+        const query = await this.buildQuery(dayOfWeekQuery, descriptionQuery);
+        return await this.repository.getQuery(query);
+    }
 
-        return await this.repository.getQuery(weekdayQuery);
+    private async getById(id: string) {
+        const event = await this.repository.getById(id);
+
+        if (!event) {
+            throw new Error('Não achou o evento não meu consagrado');
+        }
+        return event;
+    }
+
+    private async buildQuery(
+        dayOfWeekQuery: TypeDayOfWeek,
+        descriptionQuery: string,
+    ) {
+        const query: IEventQuery = {};
+
+        if (dayOfWeekQuery) query['dayOfWeek'] = dayOfWeekQuery;
+        if (descriptionQuery) {
+            const regex = new RegExp(`.*${descriptionQuery}.*`, 'i');
+            query['description'] = { $regex: regex };
+        }
+
+        return query;
     }
 
     public async createEvent(payload: IEvent) {
@@ -38,8 +58,7 @@ export class EventService {
 
     public async remove(req: Request) {
         const id = req.params.id;
-        const weekdayQuery: TypeDayOfWeek = req.query
-            .dayOfWeek as TypeDayOfWeek;
+        const dayOfWeekQuery: IEventQuery = req.query.dayOfWeek as IEventQuery;
 
         if (id) {
             const deletedEvent = await this.repository.removeById(id);
@@ -50,8 +69,8 @@ export class EventService {
             return null;
         }
 
-        if (!weekdayQuery) throw new Error('sem query meu patrão');
+        if (!dayOfWeekQuery) throw new Error('sem query meu patrão');
 
-        return await this.repository.removeByDay(weekdayQuery);
+        return await this.repository.removeByDay(dayOfWeekQuery);
     }
 }
