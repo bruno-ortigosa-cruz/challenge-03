a6f9f1e8c95c93e5e80d808c7d980fb46e728b87
Bruno Ortigosa Cruz <brunortigosa@gmail.com>
2023-12-30 01:50:16 -0300

    feat: implement error handling to the routes

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,16 @@ import { EventRoutes } from './infra/routes/event.route';
 import { UserRoutes } from './infra/routes/user.route';
 import { EndpointNotFound } from './helpers/middlewares/not-found.middleware';
 import { AuthMiddleware } from './helpers/middlewares/auth.middleware';
+import { ErrorHandlerMiddleware } from './helpers/middlewares/error-handler.middleware';
+import { ErrorTriggerMiddleware } from './helpers/middlewares/error-trigger.middleware';
 
 export class App {
     public readonly app = express();
     private eventRoute: EventRoutes;
     private userRoute: UserRoutes;
     private authentication: AuthMiddleware;
+    private errorTrigger: ErrorTriggerMiddleware;
+    private errorHandler: ErrorHandlerMiddleware;
     private prefix: string;
 
     constructor(prefix: string) {
@@ -17,6 +21,8 @@ export class App {
         this.userRoute = new UserRoutes();
         this.eventRoute = new EventRoutes();
         this.authentication = new AuthMiddleware();
+        this.errorTrigger = new ErrorTriggerMiddleware();
+        this.errorHandler = new ErrorHandlerMiddleware();
 
         this.app.use(express.json());
 
@@ -27,6 +33,8 @@ export class App {
             this.eventRoute.router,
         );
 
+        this.app.use(this.errorTrigger.exec);
+        this.app.use(this.errorHandler.exec);
         this.app.use(new EndpointNotFound().fallback);
     }
 }
