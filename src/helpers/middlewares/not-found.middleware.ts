c39a1c041794313e3b00850030dcb88a609b1e10
Bruno Ortigosa Cruz <brunortigosa@gmail.com>
2023-12-30 01:32:34 -0300

    refactor: remove redundant constructor

--- src/helpers/middlewares/not-found.middleware.ts
+++ src/helpers/middlewares/not-found.middleware.ts
@@ -2,8 +2,6 @@ import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 export class EndpointNotFound {
-    constructor() {}
-
     public fallback(_: Request, res: Response) {
         res.status(StatusCodes.NOT_FOUND).json({
             message: 'Route does not exist',
