372c06746f712ecbfaa14a2c55fa1b31910eaa28
Bruno Ortigosa Cruz <brunortigosa@gmail.com>
2024-01-01 02:45:04 -0300

    feat: update fallback response

--- src/helpers/middlewares/fallback.middleware.ts
+++ src/helpers/middlewares/fallback.middleware.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
-export class EndpointNotFound {
-    public fallback(_: Request, res: Response) {
-        res.status(StatusCodes.NOT_FOUND).json({
+export class FallbackMiddleware {
+    public exec(_: Request, res: Response) {
+        const message = {
+            statusCode: StatusCodes.NOT_FOUND,
             message: 'Route does not exist',
-        });
+            error: 'Not Found',
+        };
+
+        res.status(StatusCodes.NOT_FOUND).json(message);
     }
 }
