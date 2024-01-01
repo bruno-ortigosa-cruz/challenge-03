2c5679aa4302bd883093019a04733d78102e6785
Bruno Ortigosa Cruz <brunortigosa@gmail.com>
2024-01-01 02:45:58 -0300

    feat: implement validation

--- src/infra/routes/event.route.ts
+++ src/infra/routes/event.route.ts
@@ -1,21 +1,30 @@
 import { Router } from 'express';
 import { EventController } from '../controllers/event.controller';
+import { ValidationMiddleware } from '../../helpers/middlewares/validation.middleware';
+import { createEventSchema } from '../../helpers/validations/event.validation';
+import { IRoute } from '../../helpers/interfaces/route.interface';
 
-export class EventRoutes {
+export class EventRoutes implements IRoute {
     public readonly router: Router = Router();
     private controller: EventController;
+    private validation: ValidationMiddleware;
     private readonly generalRoute = '/events';
     private readonly idRoute = '/events/:id';
 
     constructor() {
         this.controller = new EventController();
+        this.validation = new ValidationMiddleware();
         this.setRoute();
     }
 
     private setRoute(): void {
         this.router.get(this.generalRoute, this.controller.get);
         this.router.get(this.idRoute, this.controller.get);
-        this.router.post(this.generalRoute, this.controller.create);
+        this.router.post(
+            this.generalRoute,
+            this.validation.exec(createEventSchema),
+            this.controller.create,
+        );
         this.router.delete(this.generalRoute, this.controller.remove);
         this.router.delete(this.idRoute, this.controller.remove);
     }
--- src/infra/routes/user.route.ts
+++ src/infra/routes/user.route.ts
@@ -1,20 +1,36 @@
 import { Router } from 'express';
 import { UserController } from '../controllers/user.controller';
+import { ValidationMiddleware } from '../../helpers/middlewares/validation.middleware';
+import {
+    userSignInSchema,
+    userSignUpSchema,
+} from '../../helpers/validations/user.validation';
+import { IRoute } from '../../helpers/interfaces/route.interface';
 
-export class UserRoutes {
+export class UserRoutes implements IRoute {
     public readonly router: Router = Router();
     private controller: UserController;
+    private validation: ValidationMiddleware;
     private readonly generalRoute = '/users';
     private readonly signUpRoute = `${this.generalRoute}/sign-up`;
     private readonly signInRoute = `${this.generalRoute}/sign-in`;
 
     constructor() {
         this.controller = new UserController();
+        this.validation = new ValidationMiddleware();
         this.setRoute();
     }
 
     private setRoute(): void {
-        this.router.post(this.signUpRoute, this.controller.signUp);
-        this.router.post(this.signInRoute, this.controller.signIn);
+        this.router.post(
+            this.signUpRoute,
+            this.validation.exec(userSignUpSchema),
+            this.controller.signUp,
+        );
+        this.router.post(
+            this.signInRoute,
+            this.validation.exec(userSignInSchema),
+            this.controller.signIn,
+        );
     }
 }
