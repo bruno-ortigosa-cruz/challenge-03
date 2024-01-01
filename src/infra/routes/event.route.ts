b9237547b0a427ee892e77e159c944f4bf7a2f5b
Bruno Ortigosa Cruz <brunortigosa@gmail.com>
2024-01-01 18:18:21 -0300

    feat: add query validation

--- src/infra/routes/event.route.ts
+++ src/infra/routes/event.route.ts
@@ -3,6 +3,10 @@ import { EventController } from '../controllers/event.controller';
 import { ValidationMiddleware } from '../../helpers/middlewares/validation.middleware';
 import { createEventSchema } from '../../helpers/validations/event.validation';
 import { IRoute } from '../../helpers/interfaces/route.interface';
+import {
+    deleteEventQuery,
+    getEventQuery,
+} from '../../helpers/validations/query.validation';
 
 export class EventRoutes implements IRoute {
     public readonly router: Router = Router();
@@ -18,14 +22,22 @@ export class EventRoutes implements IRoute {
     }
 
     private setRoute(): void {
-        this.router.get(this.generalRoute, this.controller.get);
+        this.router.get(
+            this.generalRoute,
+            this.validation.exec(getEventQuery, true),
+            this.controller.get,
+        );
         this.router.get(this.idRoute, this.controller.get);
         this.router.post(
             this.generalRoute,
             this.validation.exec(createEventSchema),
             this.controller.create,
         );
-        this.router.delete(this.generalRoute, this.controller.remove);
+        this.router.delete(
+            this.generalRoute,
+            this.validation.exec(deleteEventQuery, true),
+            this.controller.remove,
+        );
         this.router.delete(this.idRoute, this.controller.remove);
     }
 }
