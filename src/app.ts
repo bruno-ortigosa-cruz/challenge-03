b1ca4ae8a642e1d1ddade802331fc3dadb0bd3b0
Bruno Ortigosa Cruz <brunortigosa@gmail.com>
2024-01-01 02:47:22 -0300

    feat: make routes more dynamic

--- src/app.ts
+++ src/app.ts
@@ -2,39 +2,39 @@ import 'express-async-errors';
 import express from 'express';
 import { EventRoutes } from './infra/routes/event.route';
 import { UserRoutes } from './infra/routes/user.route';
-import { EndpointNotFound } from './helpers/middlewares/not-found.middleware';
+import { FallbackMiddleware } from './helpers/middlewares/fallback.middleware';
 import { AuthMiddleware } from './helpers/middlewares/auth.middleware';
 import { ErrorHandlerMiddleware } from './helpers/middlewares/error-handler.middleware';
-import { ErrorTriggerMiddleware } from './helpers/middlewares/error-trigger.middleware';
+import { IApp } from './helpers/interfaces/route.interface';
 
-export class App {
+export class App implements IApp {
     public readonly app = express();
-    private eventRoute: EventRoutes;
-    private userRoute: UserRoutes;
+    public routes;
     private authentication: AuthMiddleware;
-    private errorTrigger: ErrorTriggerMiddleware;
+    private fallback: FallbackMiddleware;
     private errorHandler: ErrorHandlerMiddleware;
     private prefix: string;
 
     constructor(prefix: string) {
         this.prefix = prefix;
-        this.userRoute = new UserRoutes();
-        this.eventRoute = new EventRoutes();
+        this.routes = {
+            user: new UserRoutes(),
+            event: new EventRoutes(),
+        };
         this.authentication = new AuthMiddleware();
-        this.errorTrigger = new ErrorTriggerMiddleware();
         this.errorHandler = new ErrorHandlerMiddleware();
+        this.fallback = new FallbackMiddleware();
 
         this.app.use(express.json());
 
-        this.app.use(this.prefix, this.userRoute.router);
+        this.app.use(this.prefix, this.routes.user.router);
         this.app.use(
             this.prefix,
             this.authentication.exec,
-            this.eventRoute.router,
+            this.routes.event.router,
         );
 
-        this.app.use(this.errorTrigger.exec);
         this.app.use(this.errorHandler.exec);
-        this.app.use(new EndpointNotFound().fallback);
+        this.app.use(this.fallback.exec);
     }
 }
