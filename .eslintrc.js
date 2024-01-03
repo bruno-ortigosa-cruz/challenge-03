5247bad62882b5ac77009e5a5496685ee7e79162
Bruno Ortigosa Cruz <brunortigosa@gmail.com>
2024-01-03 01:08:22 -0300

    chore: remove eslint from module imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
             'error',
             { argsIgnorePattern: '^_' },
         ],
+        '@typescript-eslint/no-var-requires': 'off',
     },
     ignorePatterns: ['**/*.js'],
 };
