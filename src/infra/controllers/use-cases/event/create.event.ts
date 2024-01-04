d76ae8a3ceb28ed18b089c92873c4b93d4fa0db0
Bruno Ortigosa Cruz <brunortigosa@gmail.com>
2024-01-04 00:22:57 -0300

    feat: add create event use case

---
+++ src/infra/controllers/use-cases/event/create.event.ts
@@ -0,0 +1,21 @@
+import { Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { CreateEventUseCaseSer } from '../../../../domains/use-cases/event/create.event';
+import { RequestWithUser } from '../../../../helpers/middlewares/auth.middleware';
+
+export class CreateEventUseCaseCon {
+    private service: CreateEventUseCaseSer;
+
+    constructor() {
+        this.service = new CreateEventUseCaseSer();
+        this.exec = this.exec.bind(this);
+    }
+
+    public async exec(req: RequestWithUser, res: Response) {
+        const user = req.user;
+        const body = req.body;
+        const id = req.user?._id;
+        const event = await this.service.exec(user, body, id);
+        res.status(StatusCodes.CREATED).json(event);
+    }
+}
