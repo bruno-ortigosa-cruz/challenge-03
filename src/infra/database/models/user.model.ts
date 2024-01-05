f5324201608447058f8fe42dd0c9e61bbc78a88d
Bruno Ortigosa Cruz <brunortigosa@gmail.com>
2024-01-05 02:18:50 -0300

    feat: modularize pre save logic to facilitate testing

--- src/infra/database/models/user.model.ts
+++ src/infra/database/models/user.model.ts
@@ -1,18 +1,38 @@
-import { CallbackError, model } from 'mongoose';
+import {
+    CallbackError,
+    CallbackWithoutResultAndOptionalError,
+    model,
+} from 'mongoose';
 import { IUser } from '../../../helpers/interfaces/user.interface';
 import { UserSchema } from '../schemas/user.schema';
 import bcrypt from 'bcrypt';
 
-UserSchema.pre('save', async function (next) {
-    try {
+export class hashPassword {
+    public static async exec(password: string): Promise<string> {
         const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(this.password as string, salt);
-        this.password = hashedPassword;
-        
-        next();
-    } catch (exception) {
-        return next(exception as CallbackError);
+
+        const hashedPassword = await bcrypt.hash(password, salt);
+        return hashedPassword;
     }
+}
+
+export class PreSaveLogic {
+    public static async exec(
+        schema: IUser,
+        next: CallbackWithoutResultAndOptionalError,
+    ) {
+        try {
+            schema.password = await hashPassword.exec(schema.password);
+
+            next();
+        } catch (exception) {
+            return next(exception as CallbackError);
+        }
+    }
+}
+
+UserSchema.pre('save', async function (next) {
+    await PreSaveLogic.exec(this, next);
 });
 
 export const UserModel = model<IUser>('User', UserSchema);
